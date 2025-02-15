19d4c014674d734e394592c9e9a386a192694768
JustinLi <kobs666666@gmail.com>
2025-02-15 18:30:18 +0800

    feat: add heroUI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
+const { heroui } = require("@heroui/react");
+
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}","./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 };
